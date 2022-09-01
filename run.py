410cf3158d484d6a309ea70914e48a66cd68a743
Garg, Nikhil <Nikhil.Garg@data61.csiro.au>
2022-09-01 13:10:07 +1000

    updated to ubuntu 20.04 and expanded the test to check geostack installation

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
-import geostack
-print("Geostack is loaded!")
+try:
+    import geostack
+    from geostack.raster import Raster
+    from geostack.runner import runScript
+
+    testA = Raster(name='testA')
+    testA.init(nx=256, hx=1.0, ny=256.0, hy=1.0)
+    runScript("testA = randomNormal(0, 1);", [testA])
+
+    print("Geostack is loaded!")
+except Exception as _:
+    print("Geostack installation doesn't work")
+    raise RuntimeError("failed")
